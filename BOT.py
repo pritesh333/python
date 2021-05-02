40965d5fc29f9d2ccaca478a7638f85a935b48d8
dragbot_333 <82358151+pritesh333@users.noreply.github.com>
2021-05-02 23:43:57 +0530

    Update BOT.py

--- BOT.py
+++ BOT.py
@@ -1,3 +1,4 @@
 import pywhatkit
 
-pywhatkit.sendwhatmsg("+919420153046","This is an automated message using python",12,35)
+pywhatkit.sendwhatmsg("(+countrycodeyournumber)","This is an automated message using python",12,35)
+# eg of a number +921234567890
